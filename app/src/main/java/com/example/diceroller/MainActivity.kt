994564e26ceb43e3dc28a5dc668f826c3a1dccf5
golds <goldsten23@gmail.com>
2022-05-16 17:49:13 +0300

    add: button, textview, random number

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
+import java.util.*
 
 /*
 * MainActivity наследует/расширяет AppCompatActivity()
@@ -21,6 +25,25 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(s)
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
+		// Получаем  ссылку на кнопку и присваиваем ее неизменяемой val переменной с именем rollButton
+		val rollButton: Button = binding.rollButton
+		rollButton.setOnClickListener {
+			// для этого метода требуются 3 вещи
+			// - class, называемый context(^C)
+			// - "message"
+			// - duration
+			Toast.makeText(this, "Btn Click", Toast.LENGTH_SHORT).show()
+
+			rollDice()
+		}
+
+	}
+
+	private fun rollDice() {
+		val resultText: TextView = binding.textView
+		// (рандомность будет от 0 - 5, что бы было 1 - 6, нужно +1)
+		val randomInt = Random().nextInt(6) + 1
+		resultText.text = randomInt.toString()
 	}
 }
 /*
@@ -29,5 +52,6 @@ class MainActivity : AppCompatActivity() {
 * Во время расширение макета view(представления), определенные в файлах макета xml, превращаются или расширяются в View Objects(объкты представления) Kotlin в память
 * Зате MainActivity может начать рисовать эти View Object (объекты представления) на экране
 *
-*
+* (^C) - Object context позволяет общаться и получать информацию о текущем состоянии системы Android
+* MainActivity - является подклассом context
 */
